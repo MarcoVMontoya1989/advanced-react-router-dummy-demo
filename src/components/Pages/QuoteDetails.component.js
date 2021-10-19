88bd525424855234a4b3163e6f622d41a68d1277
Marco Antonio V Montoya <marcovmontoya@gmail.com>
2021-10-19 15:00:07 -0400

    feat: improvisation for better path url and load comments

--- src/components/Pages/QuoteDetails.component.js
+++ src/components/Pages/QuoteDetails.component.js
@@ -1,12 +1,12 @@
 import React, {Fragment} from 'react';
-import {useParams, Route} from 'react-router-dom';
+import {useParams, Route, Link, useRouteMatch} from 'react-router-dom';
 import Comments from '../comments/Comments';
 import HighlightedQuote from '../quotes/HighlightedQuote';
 
 import DUMMY_QUOTES from "../tempData";
 
 const QuoteDetailsComponent = () => {
-
+  const matchUrl = useRouteMatch();
   const params = useParams();
 
   let quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
@@ -17,9 +17,15 @@ const QuoteDetailsComponent = () => {
 
   return (
     <Fragment>
-      <h1>quote details</h1>
-      <p>{params.quoteId}</p>
       <HighlightedQuote data={quote} />
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className="centered">
+          <Link className='btn--flat'
+                to={`/quotes/${params.quoteId}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
       <Route path={`/quotes/${params.quoteId}/comments`}
              component={Comments}>
       </Route>
